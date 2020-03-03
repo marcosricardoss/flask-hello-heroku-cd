c0eb7889d91d7ec1126dd83218bb6b1b894da269
Marcos Ricardo <marcosricardoss@gmail.com>
2020-03-03 08:56:47 -0300

    test: add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import tempfile
+
+import pytest
+from app import create_app
+
+@pytest.fixture
+def app():
+    app = create_app()
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
